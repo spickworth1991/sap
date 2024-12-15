a9326140a975742cc844e787c851f6dc402f73f4
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-15 18:05:38 -0500

    entry updated success

--- api/errors.js
+++ api/errors.js
@@ -1,9 +1,9 @@
 // errors.js
 module.exports = {
   NO_INPUT_PROVIDED: { code: 1001, message: "No input provided." },
-  PUNCH_IN_EXISTS: { code: 1002, message: "Already punched in on this date." },
-  NO_PUNCH_IN_FOUND: { code: 1003, message: "No Punch In time found for today." },
-  PUNCH_OUT_EXISTS: { code: 1004, message: "Already punched out on this date." },
+  PUNCH_IN_EXISTS: { code: 1002, message: "Already punched in today!" },
+  NO_PUNCH_IN_FOUND: { code: 1003, message: "No Punch In time found for today!" },
+  PUNCH_OUT_EXISTS: { code: 1004, message: "Already punched out today!" },
   NO_ENTRY_FOUND: { code: 1005, message: "No entry found for this date." },
   INVALID_DATE_FORMAT: { code: 1006, message: "Invalid date format provided." },
   UPDATE_FAILED: { code: 1007, message: "Failed to update the entry." },
