37e769ca6ac6a9fcf6e07e4064c94590f2ac1723
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-17 20:56:07 -0500

    closer

--- public/script.js
+++ public/script.js
@@ -48,6 +48,34 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    function navigate(pageId) {
+        showPage(pageId);
+    }
+    
+    // Function to update status and hide it after a certain duration
+    function updateStatus(message, type) {
+        const statusBox = document.getElementById("statusBox");
+    
+        if (statusBox) {
+            if (typeof message === 'object' && message.code && message.message) {
+                statusBox.innerText = `${type === 'error' ? 'Error' : 'Success'} ${message.code}: ${message.message}`;
+            } else if (typeof message === 'string') {
+                statusBox.innerText = message;
+            } else {
+                statusBox.innerText = type === 'error' ? 'An unknown error occurred.' : 'Operation successful.';
+            }
+    
+            statusBox.className = type;
+            statusBox.classList.add("show");
+            console.log("statusBox.classList", statusBox.classList);
+            // Hide the status box after a delay
+            setTimeout(() => {
+                statusBox.classList.remove("show");
+            }, type === "success" ? 3000 : 5000);
+        }
+    }
+    
+
     // Logout functionality
     if (logoutBtn) {
         logoutBtn.addEventListener('click', () => {
