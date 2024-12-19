ed80e5f8836d1510516837239565c6fcc9af6bfe
Pickworth <spckworth1991@gmail.com>
2024-12-18 20:36:28 -0500

    test1

--- public/script.js
+++ public/script.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const userRole = localStorage.getItem('role');
     const spreadsheetId = localStorage.getItem('spreadsheetId');
     if (userRole) {
-        loginPage.style.display = 'none';
-        homePage.style.display = 'block';
+        loginPage.style.display = 'block';
+        homePage.style.display = 'none';
         if (userRole === 'admin') {
             adminHomeBtn.style.display = 'inline-block';
         }
