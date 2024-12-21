b1fde034b8e4ecc399730e873fff86b16fa78496
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-21 10:50:48 -0500

    quick

--- public/login.js
+++ public/login.js
@@ -35,3 +35,15 @@ if (loginForm) {
         }
     });
 }
+
+
+// Logout functionality
+const logoutBtn = document.getElementById('logout-btn')
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => {
+        localStorage.removeItem('role');
+        localStorage.removeItem('spreadsheetId');
+        window.location.href = 'index.html'
+        adminHomeBtn.style.display = 'none';
+    });
+}
