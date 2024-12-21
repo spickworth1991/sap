2532f6805a5a122651c7a8544d2b96fc526553bb
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-21 10:58:32 -0500

    who knows

--- api/index.js
+++ api/index.js
@@ -6,10 +6,15 @@ require('dotenv').config();
 // Middleware and Routes
 const { validateSpreadsheetId } = require('../middleware/validate');
 const { logAction } = require('../middleware/log');
+const { loginForm } = require('../routes/login');
+const { logoutBtn } = require('../routes/logout');
+
+
 const punchRoutes = require('../routes/punch');
 const sapRoutes = require('../routes/sap');
 const editRoutes = require('../routes/edit');
 const logsRoutes = require('../routes/logs');
+
 
 // Import utilities
 const { getGoogleSheetsService, getCurrentDate, getCurrentTime, getCurrentMonthName } = require('../utils/googleSheetsUtils');
@@ -22,12 +27,15 @@ app.use(express.json());
 app.use(cors());
 app.use(validateSpreadsheetId);
 app.use(logAction);
+app.use(loginForm)
+app.use(logoutBtn)
 
 // Modular Routes
 app.use('/api/punch', punchRoutes);
 app.use('/api/sap', sapRoutes);
 app.use('/api/editEntry', editRoutes);
 app.use('/api/logs', logsRoutes);
+
 
  
   // Start the Server
