49befe2dabda7283cfd53967f5f90582fd28cdfe
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-21 10:47:22 -0500

    back an index

--- api/index.js
+++ api/index.js
@@ -6,16 +6,10 @@ require('dotenv').config();
 // Middleware and Routes
 const { validateSpreadsheetId } = require('../middleware/validate');
 const { logAction } = require('../middleware/log');
-const { loginForm } = require('../routes/login');
-const { logoutBtn } = require('../routes/logout');
-
-
 const punchRoutes = require('../routes/punch');
 const sapRoutes = require('../routes/sap');
 const editRoutes = require('../routes/edit');
 const logsRoutes = require('../routes/logs');
-const logoutRoutes = require('../routes/logout');
-const loginRoutes = require('../routes/login');
 
 // Import utilities
 const { getGoogleSheetsService, getCurrentDate, getCurrentTime, getCurrentMonthName } = require('../utils/googleSheetsUtils');
@@ -28,16 +22,12 @@ app.use(express.json());
 app.use(cors());
 app.use(validateSpreadsheetId);
 app.use(logAction);
-app.use(loginForm)
-app.use(logoutBtn)
 
 // Modular Routes
 app.use('/api/punch', punchRoutes);
 app.use('/api/sap', sapRoutes);
 app.use('/api/editEntry', editRoutes);
 app.use('/api/logs', logsRoutes);
-app.use('/api/logout', logoutRoutes)
-app.use('/api/login', loginRoutes)
 
  
   // Start the Server
