671c733eb7f9e9bbeb4ea967babbe523894f1235
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-22 18:08:08 -0500

    test

--- public/script.js
+++ public/script.js
@@ -1,54 +1,6 @@
 // Global Scripts
 
 
-function decodeToken(token) {
-    try {
-        const payloadBase64 = token.split('.')[1];
-        const decodedPayload = atob(payloadBase64);
-        return JSON.parse(decodedPayload);
-    } catch (error) {
-        console.error('Error decoding token:', error);
-        return null;
-    }
-}
-
-function setUserDetails(authToken) {
-    const userData = decodeToken(authToken);
-    if (userData) {
-        localStorage.setItem('role', userData.role);
-        localStorage.setItem('spreadsheetId', userData.spreadsheetId);
-    } else {
-        console.error('Failed to decode user details from token.');
-    }
-}
-
-function showInitialPage() {
-    const authToken = localStorage.getItem('authToken');
-    if (authToken) {
-        setUserDetails(authToken);
-    }
-
-    const role = localStorage.getItem('role');
-    console.log(`userRole: ${role}`);
-
-    // Check the current page URL
-    const currentPage = window.location.pathname;
-    const adminHomeBtn = document.getElementById('admin-home-btn');
-
-    if (role) {
-        // Redirect to homePage.html if not already there
-        if (currentPage !== '/homePage.html') {
-            window.location.href = 'homePage.html';
-        } else if (role === 'admin' && adminHomeBtn) {
-            adminHomeBtn.style.display = 'inline-block';
-        }
-    } else {
-        // Redirect to index.html for non-admin users
-        if (currentPage !== '/index.html' && currentPage !== '/') {
-            window.location.href = 'index.html';
-        }
-    }
-}
 
 // Navigation function to show the selected page
 function navigateTo(pageId) {
@@ -80,27 +32,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
-// Function to update status and hide it after a certain duration
-function updateStatus(message, type) {
-    const statusBox = document.getElementById("statusBox");
-
-    if (statusBox) {
-        statusBox.classList.remove("success", "error");
-        statusBox.classList.add(type);
-
-        if (typeof message === 'object' && message.code && message.message) {
-            statusBox.innerText = `${type === 'error' ? 'Error' : 'Success'} ${message.code}: ${message.message}`;
-        } else if (typeof message === 'string') {
-            statusBox.innerText = message;
-        } else {
-            statusBox.innerText = type === 'error' ? 'An unknown error occurred.' : 'Operation successful.';
-        }
-
-        statusBox.classList.add("show");
-
-        setTimeout(() => {
-            statusBox.classList.remove("show");
-        }, type === "success" ? 3000 : 5000);
-    }
-}
