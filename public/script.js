69dfe52d9f590aa68138cd37c9fcdfdb68abbb43
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-22 16:23:25 -0500

    test

--- public/script.js
+++ public/script.js
@@ -1,32 +1,57 @@
 // Global Scripts
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    // Run the function only on index.html or homePage.html
+    // Run the function only on specific pages
     const currentPage = window.location.pathname;
-    if (currentPage === '/index.html' || currentPage === '/homePage.html'|| currentPage === '/') {
+    if (currentPage === '/index.html' || currentPage === '/homePage.html' || currentPage === '/') {
         showInitialPage();
     }
 });
 
+function decodeToken(token) {
+    try {
+        const payloadBase64 = token.split('.')[1];
+        const decodedPayload = atob(payloadBase64);
+        return JSON.parse(decodedPayload);
+    } catch (error) {
+        console.error('Error decoding token:', error);
+        return null;
+    }
+}
+
+function setUserDetails(authToken) {
+    const userData = decodeToken(authToken);
+    if (userData) {
+        localStorage.setItem('role', userData.role);
+        localStorage.setItem('spreadsheetId', userData.spreadsheetId);
+    } else {
+        console.error('Failed to decode user details from token.');
+    }
+}
+
 function showInitialPage() {
-    const adminHomeBtn = document.getElementById('admin-home-btn');
-    const userRole = localStorage.getItem('authToken');
-    console.log(`userRole: ${userRole}`);
+    const authToken = localStorage.getItem('authToken');
+    if (authToken) {
+        setUserDetails(authToken);
+    }
+
+    const role = localStorage.getItem('role');
+    console.log(`userRole: ${role}`);
 
     // Check the current page URL
     const currentPage = window.location.pathname;
+    const adminHomeBtn = document.getElementById('admin-home-btn');
 
-    if (userRole) {
-        // If the user is logged in and not on the home page, redirect to homePage.html
+    if (role === 'admin') {
+        // Redirect to homePage.html if not already there
         if (currentPage !== '/homePage.html') {
             window.location.href = 'homePage.html';
-        } else if (userRole === 'admin' && adminHomeBtn) {
+        } else if (adminHomeBtn) {
             adminHomeBtn.style.display = 'inline-block';
         }
     } else {
-        // If no user role and not on the login page, redirect to index.html
-        if (currentPage !== '/index.html'|| currentPage === '/') {
+        // Redirect to index.html for non-admin users
+        if (currentPage !== '/index.html' && currentPage !== '/') {
             window.location.href = 'index.html';
         }
     }
@@ -34,36 +59,24 @@ function showInitialPage() {
 
 // Navigation function to show the selected page
 function navigateTo(pageId) {
-    switch (pageId) {
-        case 'homePage':
-            window.location.href = 'homePage.html';
-            break;
-        case 'manageUsersPage':
-            window.location.href = 'manage_users.html';
-            break;
-        case 'viewLogsPage':
-            window.location.href = 'view_logs.html';
-            break;
-        case 'editEntriesPage':
-            window.location.href = 'edit_entries.html';
-            break;
-        case 'clockPage':
-            window.location.href = 'clockPage.html';
-            break;
-        case 'sapPage':
-            window.location.href = 'sapPage.html';
-            break;
-        case 'dateSelectionPage':
-            window.location.href = 'dateSelect.html';
-            break;
-        case 'adminPage':
-            window.location.href = 'admin.html';
-            break;
-        default:
-            console.log(`Page not found: ${pageId}`);
+    const pageMap = {
+        homePage: 'homePage.html',
+        manageUsersPage: 'manage_users.html',
+        viewLogsPage: 'view_logs.html',
+        editEntriesPage: 'edit_entries.html',
+        clockPage: 'clockPage.html',
+        sapPage: 'sapPage.html',
+        dateSelectionPage: 'dateSelect.html',
+        adminPage: 'admin.html',
+    };
+
+    const pageUrl = pageMap[pageId];
+    if (pageUrl) {
+        window.location.href = pageUrl;
+    } else {
+        console.log(`Page not found: ${pageId}`);
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const logoutBtn = document.getElementById('logout-btn');
@@ -75,9 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-
 // Function to update status and hide it after a certain duration
-
 function updateStatus(message, type) {
     const statusBox = document.getElementById("statusBox");
 
