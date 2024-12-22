19e4f1d9e86c72067aae2f23cc6a1468f9758381
Pickworth <spckworth1991@gmail.com>
2024-12-22 00:13:24 -0500

    h

--- middleware/logSetup.js
+++ middleware/logSetup.js
@@ -1,18 +1,8 @@
-
-const fs = require('fs');
-const path = require('path');
 const morgan = require('morgan');
 
-// Create a write stream (in append mode) for logging
-const logDirectory = path.join(__dirname, '../logs');
-if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory);
-}
-const accessLogStream = fs.createWriteStream(path.join(logDirectory, 'access.log'), { flags: 'a' });
-
-// Setup the logger
+// Setup the logger to log to the console
 const setupLogging = (app) => {
-    app.use(morgan('combined', { stream: accessLogStream }));
+    app.use(morgan('combined'));
 };
 
 module.exports = { setupLogging };
