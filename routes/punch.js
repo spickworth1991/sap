8e7ec69ebfa4fab2b41358c9df0ba6d603d43ad5
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-24 17:16:51 -0500

    Testing

--- routes/punch.js
+++ routes/punch.js
@@ -18,7 +18,7 @@ const router = express.Router();
 const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 // Punch-in route
 router.post('/in', async (req, res) => {
-    await fetchSpreadsheetId;
+fetchSpreadsheetId;
     
     try {
 
