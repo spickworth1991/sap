8928cea8e8ca483af6485bca41da7046d7ac21b6
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-26 11:13:13 -0500

    Testing

--- public/dateSelect.js
+++ public/dateSelect.js
@@ -1,5 +1,15 @@
 
-function fetchEntriesByDate() {
+document.addEventListener('DOMContentLoaded', () => {
+    const dateInput = document.getElementById('fetchUserEntries');
+    if (dateInput) {
+      dateInput.addEventListener('click', () => fetchEntriesByDate(dateInput)); // Attach click event
+    } else {
+        console.error('Sap Input Error at button.');
+    }
+});
+
+
+async function fetchEntriesByDate(button) {
     const dateInput = document.getElementById('datePicker').value;
     if (!dateInput) {
         updateStatus("Please select a date.", "error");
