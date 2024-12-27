bb18ecdef882894ab2fa491211f27410fe364f6d
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2024-12-26 20:54:02 -0500

    Testing

--- public/editEntryUser.js
+++ public/editEntryUser.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 export async function editEntryHandler(editEntry) {
     const button = event.target;
     const rowNumber = button.getAttribute('data-row-number');
-    const date = document.getElementById('date').innerText;
+    const date = button.closest('td').querySelector('tr').innerText;
     console.log(`date: ${date}`);
     const newTime = prompt('Enter new time (HH:mm:ss):');
     const newProjectActivity = prompt('Enter new project/activity:');
